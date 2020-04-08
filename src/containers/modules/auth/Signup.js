c41cfee7338ffefb4a0d32673c28d52b0cf4b350
Jure <jure.djakovic@student.um.si>
2020-04-08 16:03:59 +0200

    Add frontend registration UI

--- src/containers/modules/auth/Signup.js
+++ src/containers/modules/auth/Signup.js
@@ -1,20 +1,121 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-
+import backgroundImage from '../../../assets/background.jpg';
+import formImage from '../../../assets/form-image.jpg';
 
 const styles = theme => ({
 
   root: {
+    width: '100vw',
+    height: '100vh',
   },
+
+  image: {
+    width: '100vw',
+    height: '100vh',
+    backgroundImage : `url(${backgroundImage})`,
+    backgroundSize : 'cover',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  formContainer: {
+    display: 'flex',
+    width: '800px',
+    height: '500px',
+  },
+
+    formWrapper: {
+    width: '80%',
+    display: 'flex',
+    justifyContent: 'center',
+  },
+
+  formImage: {
+    position: 'relative',
+    width: '100%',
+    height: '100%',
+    background :`linear-gradient(180deg, rgba(255, 255, 255, 0.3), rgba(255, 255, 255, 0.6)), url(${formImage})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: '35% 40%',
+    borderRadius: '5px 0 0 5px',
+    textAlign: 'center',
+    lineHeight: '3em',
+    color: 'white',
+    transform: 'scaleY(1.5, 1.5)'
+  },
+
+  formImageText: {
+    fontFamily: 'Righteous',
+    position: 'absolute',
+    top: '1rem',
+  },
+
+  formDataContainer: {
+    width: '100%',
+    fontFamily: 'Righteous',
+    backgroundColor: 'white',
+    borderRadius: '0 5px 5px 0'
+  },
+
+  input: {
+    textAlign: 'center',
+    width: '80%',
+    margin: '2rem',
+    outline: '0', 
+    border: '0',
+    borderBottom: '1px solid #bac1b8',
+  },
+
+  button:{
+    width: '80%',
+    height: '2rem',
+    margin: '2rem',
+    color: 'white',
+    border: 'none',
+    backgroundColor: '#2b303a',
+    fontFamily: 'Roboto',
+  },
+
 });
+
 
 
 const Signup = (props) => {
 
   return (
+
+    // navbar
+      
     <>
-      <p>SIGNUP COMPONENT</p>
+    <div className={props.classes.image}>
+      <div className={props.classes.formWrapper}>
+      <div className={props.classes.formContainer}>
+        <div className={props.classes.formImage}>
+          <div className={props.classes.formImageText}>
+          <h2>Get Started!</h2>
+          <h3>Sign Up And Start Chatting With Your Friends Today!</h3>
+          </div>
+        </div>
+        <div className={props.classes.formDataContainer}>
+          <h2 style={{textAlign: 'center', textTransform: 'uppercase', color: '#bac18b'}}>Register</h2>
+          <form>
+            <input type="text" placeholder='Email' className={props.classes.input}></input>
+            <input type="text" placeholder='Username' className={props.classes.input}></input>
+            <div className={props.classes.passwordContainer}>
+            <input type="password" placeholder='Password' className={props.classes.input}></input>
+            <div className={props.classes.showPassButton}>
+            </div>
+            </div>
+            <input type="text" placeholder='Confirm password' className={props.classes.input}></input>
+            <input className={props.classes.button} type="submit" value="Register"></input> 
+          </form>
+        </div>
+      </div>
+      </div>
+    </div>
     </>
   )
 }
